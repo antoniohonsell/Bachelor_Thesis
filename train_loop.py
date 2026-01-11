a5a18f500c973d4cce92dfba751a75075b4f9d4c
antonio2 <antonio.honsell@polytechnique.edu>
2026-01-11 12:28:48 +0100

    resnet_arch

---
+++ train_loop.py
@@ -0,0 +1,17 @@
+import resnet_arch
+
+def train_model(model, dataloader, criterion, optimizer, num_epochs):
+    model.train()
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for inputs, labels in dataloader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * inputs.size(0)
+        epoch_loss = running_loss / len(dataloader.dataset)
+        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}')
+    return model
+
